8ec24bf534e5197cdf1d6206744ea0553587ef9f
2derp4you <iam2derp4you@gmail.com>
2023-06-06 01:54:37 +0200

    testing...

--- frontend/src/pages/video.js
+++ frontend/src/pages/video.js
@@ -22,18 +22,18 @@ const Video = ({ user }) => {
         <HeadProvider>
             <div className="video-page">
                 <Title>{video ? video.title : "Loading..."}</Title>
-                <Meta property="og:site_name" content="Youtube HC" />
-                <Meta property="og:url" content={url} />
-                <Meta property="og:type" content="video.other" />
-                <Meta property="og:title" content={video ? video.title : ""} />
-                <Meta property="og:image" content={video ? "https://yt-api.sigve.dev/uploads/" + video.thumbnail : ""} />
-                <Meta property="og:video" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
-                <Meta property="og:video:type" content="video/mp4" />
-                <Meta property="og:video:secure_url" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
-                <Meta property="og:video:height" content="720" />
-                <Meta property="og:video:width" content="1280" />
-                <Meta property="og:image:height" content="720" />
-                <Meta property="og:image:width" content="1280" />
+                <Meta name="og:site_name" content="Youtube HC" />
+                <Meta name="og:url" content={url} />
+                <Meta name="og:type" content="video.other" />
+                <Meta name="og:title" content={video ? video.title : ""} />
+                <Meta name="og:image" content={video ? "https://yt-api.sigve.dev/uploads/" + video.thumbnail : ""} />
+                <Meta name="og:video" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
+                <Meta name="og:video:type" content="video/mp4" />
+                <Meta name="og:video:secure_url" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
+                <Meta name="og:video:height" content="720" />
+                <Meta name="og:video:width" content="1280" />
+                <Meta name="og:image:height" content="720" />
+                <Meta name="og:image:width" content="1280" />
                 <Header user={user} />
                 <div className="video-content">
                     <div className="video-container">
