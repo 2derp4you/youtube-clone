4a7dd0d1ed97b70b4f3f8abaa670e48bfa7b1417
2derp4you <iam2derp4you@gmail.com>
2023-06-06 01:03:30 +0200

    small bugg fixes, v0.2

--- frontend/src/pages/video.js
+++ frontend/src/pages/video.js
@@ -25,12 +25,13 @@ const Video = ({ user }) => {
                     <div className="video">
                         {video ? <ReactPlayer url={"https://yt-api.sigve.dev/uploads/" + video.url} controls={true} width="100%" height="100%" /> : "Loading..."}
                     </div>
+                    {video ?
                     <div className="video-info">
                         <h1 className="title">{video ? video.title : "Loading..."}</h1>
                         <h2 className="title">Uploaded by: <a href={"/profile/" + video.ownerId}>{video ? video.ownerName : "Loading..."}</a></h2>
                         <h2 className="title">Uploaded at: <span>{video ? new Date(video.createdAt).toDateString() : "Loading..."}</span></h2>
                         <h2 className="title">Description: <span>{video ? video.description : "Loading..."}</span></h2>
-                    </div>
+                    </div> : <></>}
                 </div>
             </div>
         </div>
