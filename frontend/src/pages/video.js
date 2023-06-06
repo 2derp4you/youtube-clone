12adaad84b52a752b9d33413442a1f67d5b5cddd
2derp4you <iam2derp4you@gmail.com>
2023-06-06 02:58:57 +0200

    testing...

--- frontend/src/pages/video.js
+++ frontend/src/pages/video.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { HeadProvider, Title, Meta } from 'react-head';
-import { Head } from 'react-static';
 
 import Header from "../components/header";
 import ReactPlayer from "react-player";
@@ -20,37 +19,32 @@ const Video = ({ user }) => {
     }, []);
 
     return (
-        <div className="video-page">
-            <Head>
-                <title>{video ? video.title : "Loading..."}</title>
-                <meta property="og:title" content={video ? video.title : "Loading..."} />
-                <meta property="og:description" content={video ? video.description : "Loading..."} />
-                <meta property="og:image" content={"https://yt-api.sigve.dev/uploads/" + video.thumbnail} />
-                <meta property="og:url" content={"https://yt.sigve.dev/video/" + id} />
-                <meta property="og:type" content="video" />
-                <meta property="og:video" content={"https://yt-api.sigve.dev/uploads/" + video.url} />
-                <meta property="og:video:secure_url" content={"https://yt-api.sigve.dev/uploads/" + video.url} />
-                <meta property="og:video:type" content="video/mp4" />
-                <meta property="og:video:width" content="1920" />
-                <meta property="og:video:height" content="1080" />
-                <meta property="og:video:tag" content="video" />
-            </Head>
-            <Header user={user} />
-            <div className="video-content">
-                <div className="video-container">
-                    <div className="video">
-                        {video ? <ReactPlayer url={"https://yt-api.sigve.dev/uploads/" + video.url} controls={true} width="100%" height="100%" /> : "Loading..."}
+        <HeadProvider>
+            <div className="video-page">
+                <Title>test</Title>
+                <Meta property="og:url" content="https://yt.sigve.dev" />
+                <Meta property="og:site_name" content="Youtube HC" />
+                <Meta property="og:title" content="test" />
+                <Meta property="og:description" content="test" />
+                <Meta property="og:type" content="video.other" />
+                <Meta property="og:image" content="https://yt-api.sigve.dev/uploads/1685986007373-594928337-capybara.jpg" />
+                <Header user={user} />
+                <div className="video-content">
+                    <div className="video-container">
+                        <div className="video">
+                            {video ? <ReactPlayer url={"https://yt-api.sigve.dev/uploads/" + video.url} controls={true} width="100%" height="100%" /> : "Loading..."}
+                        </div>
+                        {video ?
+                        <div className="video-info">
+                            <h1 className="title">{video ? video.title : "Loading..."}</h1>
+                            <h2 className="title">Uploaded by: <a href={"/profile/" + video.ownerId}>{video ? video.ownerName : "Loading..."}</a></h2>
+                            <h2 className="title">Uploaded at: <span>{video ? new Date(video.createdAt).toDateString() : "Loading..."}</span></h2>
+                            <h2 className="title">Description: <span>{video ? video.description : "Loading..."}</span></h2>
+                        </div> : <></>}
                     </div>
-                    {video ?
-                    <div className="video-info">
-                        <h1 className="title">{video ? video.title : "Loading..."}</h1>
-                        <h2 className="title">Uploaded by: <a href={"/profile/" + video.ownerId}>{video ? video.ownerName : "Loading..."}</a></h2>
-                        <h2 className="title">Uploaded at: <span>{video ? new Date(video.createdAt).toDateString() : "Loading..."}</span></h2>
-                        <h2 className="title">Description: <span>{video ? video.description : "Loading..."}</span></h2>
-                    </div> : <></>}
                 </div>
             </div>
-        </div>
+        </HeadProvider>
     )
 }
 
