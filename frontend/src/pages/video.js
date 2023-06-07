9dad986bbe8eae076f6e154d0d0721025505a30f
2derp4you <iam2derp4you@gmail.com>
2023-06-08 00:33:17 +0200

    v0.3

--- frontend/src/pages/video.js
+++ frontend/src/pages/video.js
@@ -21,13 +21,20 @@ const Video = ({ user }) => {
     return (
         <HeadProvider>
             <div className="video-page">
-                <Title>test</Title>
-                <Meta property="og:url" content="https://yt.sigve.dev" />
+                <Title>{video ? video.title : "Youtube HC"}</Title>
                 <Meta property="og:site_name" content="Youtube HC" />
-                <Meta property="og:title" content="test" />
-                <Meta property="og:description" content="test" />
+                <Meta property="og:url" content={window.location.href} />
                 <Meta property="og:type" content="video.other" />
-                <Meta property="og:image" content="https://yt-api.sigve.dev/uploads/1685986007373-594928337-capybara.jpg" />
+                <Meta property="og:title" content={video ? video.title : ""} />
+                <Meta property="og:image" content={video ? "https://yt-api.sigve.dev/uploads/" + video.thumbnail : ""} />
+                <Meta property="og:video" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
+                <Meta property="og:video:type" content="video/mp4" />
+                <Meta property="og:video:secure_url" content={video ? "https://yt-api.sigve.dev/uploads/" + video.url : ""} />
+                <Meta property="og:video:height" content="720" />
+                <Meta property="og:video:width" content="1280" />
+                <Meta property="og:image:height" content="720" />
+                <Meta property="og:image:width" content="1280" />
+
                 <Header user={user} />
                 <div className="video-content">
                     <div className="video-container">
